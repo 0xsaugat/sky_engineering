5788ef142ffe0e5fa02e4e0dbde50837deda60d0
rupeshdahal07 <rupyesdahal12@gmail.com>
2026-03-19 08:18:20 +0545

    ✨ feat: added user and user profile models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    phone = models.CharField(max_length=30, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
